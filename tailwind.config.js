d31cda73e5d0a548bfd1d3025d7bc7404138da3d
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-08-05 21:26:42 +0530

    added icons and themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      "theme-main": "#011627",
+      "accent-color": "#607B96",
+      "accent-sub": "#4D5BCE",
+      "accent-url": "#E99287",
+      "accent-variable": "#43D9AD",
+      "accent-underline": "#FEA55F",
+    },
   },
   plugins: [],
 };
