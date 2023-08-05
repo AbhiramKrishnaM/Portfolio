46ad95637e302e80d18130c07692185ec1e91fef
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2023-08-05 22:24:29 +0530

    added content for under construction page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
-<script setup></script>
+<script setup>
+import Underconstruction from "./components/Underconstruction.vue";
+</script>
 
 <template>
   <div
     class="min-w-full min-h-screen bg-accent-variable flex items-center justify-center"
   >
-    <h1 class="text-3xl font-semibold text-center leading-loose">
-      _Hey there my name is
-      <span class="text-6xl font-bold">_Abhiram krishna</span>,
-      <br />
-      I am trying to create something cool🤘🏾!.
-    </h1>
+    <Underconstruction />
   </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/Underconstruction.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <h1 class="text-3xl font-semibold text-center leading-loose">
+    _Hey there my name is
+    <span class="text-6xl font-bold">_Abhiram krishna</span>,
+    <br />
+    I am trying to create something cool🤘🏾!.
+  </h1>
+</template>
