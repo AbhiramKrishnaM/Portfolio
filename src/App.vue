fb96e4d4f442febb8a6f9ab7157c6a81a351cef5
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-18 18:32:39 +0530

    added footer component, moved all assets to footer component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
-<script setup>
-import { ref } from "vue";
-import Loading from "./components/loading.vue";
-
-// assets/icons
-import Twitter from "~/icons/twitter.svg";
-import Facebook from "~/icons/facebook.svg";
-import Github from "~/icons/github.svg";
-
-const showLoading = ref(true);
-</script>
-
 <template>
   <div
     class="min-w-full min-h-screen bg-theme-main text-accent-color flex items-center justify-center border border-border-white"
@@ -19,32 +7,19 @@ const showLoading = ref(true);
       @animation-complete="() => (showLoading = false)"
     />
     <div v-else class="flex flex-col w-full h-screen">
-      <router-view></router-view>
-
-      <div class="fixed bottom-0 left-0 w-full border border-border-white px-6">
-        <div class="flex items-center justify-between">
-          <div id="redirects" class="flex items-center justify-center">
-            <span class="py-3.5 pr-3.5 border-r border-border-white"
-              >find me in:</span
-            >
-
-            <a href="#" class="p-3.5 border-r border-border-white">
-              <img :src="Twitter" alt="Twitter icon" />
-            </a>
-            <a href="#" class="p-3.5 border-r border-border-white">
-              <img :src="Facebook" alt="Facebook icon" />
-            </a>
-          </div>
-
-          <div
-            id="github"
-            class="flex items-center justify-center gap-2 p-3.5 border-l border-border-white"
-          >
-            <a href="#">@AbhiramKrishnaM</a>
-            <a href="#"><img :src="Github" alt="Github icon" /></a>
-          </div>
-        </div>
+      <div class="px-6">
+        <router-view></router-view>
       </div>
+      <Footer />
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+// components
+import Loading from "./components/loading.vue";
+import Footer from "@/components/Footer.vue";
+
+const showLoading = ref(true);
+</script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>Footer section</div>
+  <div class="fixed bottom-0 left-0 w-full border border-border-white px-6">
+    <div class="flex items-center justify-between">
+      <div id="redirects" class="flex items-center justify-center">
+        <span class="py-3.5 pr-3.5 border-r border-border-white"
+          >find me in:</span
+        >
+
+        <a href="#" class="p-3.5 border-r border-border-white">
+          <img :src="Twitter" alt="Twitter icon" />
+        </a>
+        <a href="#" class="p-3.5 border-r border-border-white">
+          <img :src="Facebook" alt="Facebook icon" />
+        </a>
+      </div>
+
+      <div
+        id="github"
+        class="flex items-center justify-center gap-2 p-3.5 border-l border-border-white"
+      >
+        <a href="#">@AbhiramKrishnaM</a>
+        <a href="#"><img :src="Github" alt="Github icon" /></a>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
-
-<style scoped></style>
+<script setup>
+// assets/icons
+import Twitter from "~/icons/twitter.svg";
+import Facebook from "~/icons/facebook.svg";
+import Github from "~/icons/github.svg";
+</script>
