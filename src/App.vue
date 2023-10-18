f7c8147f368a0c72805454ff27b41ae260c52d1f
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-18 19:22:36 +0530

    added landing page content

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <div
-    class="min-w-full min-h-screen bg-theme-main text-accent-color flex items-center justify-center border border-border-white"
-  >
+  <div class="min-w-full min-h-screen flex items-center justify-center">
     <Loading
       v-if="showLoading"
       @animation-complete="() => (showLoading = false)"
     />
-    <div v-else class="flex flex-col w-full h-screen">
+    <div v-else class="flex flex-col w-full h-screen text-accent-color">
       <Navbar />
-      <div class="px-6">
+      <div class="px-6 h-full overflow-y-auto">
         <router-view></router-view>
       </div>
       <Footer />
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,28 @@
 <template>
-  <div>Hello I am abhiram krishna M</div>
+  <div class="flex items-center justify-center">
+    <div>
+      <div id="section-1">
+        <h6>Hi all, I am</h6>
+        <h1>Abhiram Krishan M</h1>
+        <span> > Full Stack Developer </span>
+      </div>
+
+      <div id="section-2">
+        <ul>
+          <li>// complete the game to continue.</li>
+          <li>// you can also see it on my github page.</li>
+        </ul>
+
+        <p>
+          const
+          <span>githubLink</span>
+          <span>=</span>
+          <a href="#">"https://github.com/example/url"</a>
+        </p>
+      </div>
+    </div>
+
+    <div id="snake-game"></div>
+  </div>
 </template>
 <script setup></script>
