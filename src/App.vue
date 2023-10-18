0518cbd92a5d908d5804b530a6ca8694a1a354c6
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-18 19:01:27 +0530

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,25 @@
       @animation-complete="() => (showLoading = false)"
     />
     <div v-else class="flex flex-col w-full h-screen">
+      <div
+        class="px-6 border-b border-border-white flex items-center justify-between"
+      >
+        <div class="flex items-center">
+          <span class="py-3.5 pr-3.5 border-r border-border-white">
+            abhiram-krishna
+          </span>
+          <ul class="flex items-center">
+            <li class="p-3.5 border-r border-border-white">_hello</li>
+            <li class="p-3.5 border-r border-border-white">_about-me</li>
+            <li class="p-3.5 border-r border-border-white">_projects</li>
+          </ul>
+        </div>
+
+        <div class="flex w-[186px]">
+          <a href="#" class="p-3.5 border-l border-border-white">_contact-me</a>
+        </div>
+      </div>
+
       <div class="px-6">
         <router-view></router-view>
       </div>
