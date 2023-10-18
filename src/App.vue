097962f85cf4ef605d82dc818499457b8c6282c5
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-18 17:37:24 +0530

    added content for footer

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,20 @@ const showLoading = ref(true);
     <div v-else class="d-flex flex-col w-full h-screen">
       <router-view></router-view>
 
-      <div class="fixed bottom-0 left-0">footer section</div>
+      <div class="fixed bottom-0 left-0">
+        <div>
+          <div id="redirects">
+            <span>Find me in:</span>
+            <a href="#"><img src="" alt="Twitter icon" /></a>
+            <a href="#"><img src="" alt="Linkedin icon" /></a>
+          </div>
+
+          <div id="github">
+            <span>@username</span>
+            <a href="#"><img src="" alt="Github icon" /></a>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
