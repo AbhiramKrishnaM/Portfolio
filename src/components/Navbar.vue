1f6fb730cd8e2ae14e24c54bad2f12e2b020dca1
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-18 19:02:51 +0530

    added navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>Navbar section</div>
+  <div
+    class="px-6 border-b border-border-white flex items-center justify-between"
+  >
+    <div class="flex items-center">
+      <span class="py-3.5 pr-3.5 border-r border-border-white">
+        abhiram-krishna
+      </span>
+      <ul class="flex items-center">
+        <li class="p-3.5 border-r border-border-white">_hello</li>
+        <li class="p-3.5 border-r border-border-white">_about-me</li>
+        <li class="p-3.5 border-r border-border-white">_projects</li>
+      </ul>
+    </div>
+
+    <div class="flex w-[186px]">
+      <a href="#" class="p-3.5 border-l border-border-white">_contact-me</a>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
-
-<style scoped></style>
