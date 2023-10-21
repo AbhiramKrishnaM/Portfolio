2acbbb577684c84ca02d68a8cd2a6049f8944922
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-21 21:11:33 +0530

    added bolts

--- src/components/SnakeGame.vue
+++ src/components/SnakeGame.vue
@@ -1,12 +1,43 @@
 <template>
-  <div id="snake-game" class="relative">
-    <img :src="Green" alt="Background halo" class="absolute -left-32 -top-32" />
+  <div id="snake-game" class="relative p-3">
+    <img
+      id="green-vector"
+      :src="Green"
+      alt="Background halo"
+      class="absolute -left-32 -top-32"
+    />
 
     <img
       :src="Blue"
       alt="Background halo"
       class="absolute -right-36 -bottom-36"
     />
+
+    <img
+      :src="BoltTopLeft"
+      alt="Screw top left"
+      class="absolute top-0 left-0 ml-3 mt-3"
+    />
+    <img
+      :src="BoltTopRight"
+      alt="Screw top right"
+      class="absolute top-0 right-0 mr-3 mt-3"
+    />
+
+    <img
+      :src="BoltDownLeft"
+      alt="Screw bottom left"
+      class="absolute bottom-0 left-0 ml-3 mb-3"
+    />
+    <img
+      :src="BoltDownRight"
+      alt="Screw bottom right"
+      class="absolute bottom-0 right-0 mr-3 mb-3"
+    />
+
+    <div class="m-6">
+      <canvas id="gameBoard" width="239" height="405" class=""></canvas>
+    </div>
   </div>
 </template>
 
@@ -14,6 +45,10 @@
 // assets
 import Green from "~/vectors/Green.svg";
 import Blue from "~/vectors/Blue.svg";
+import BoltDownLeft from "~/icons/bolt-down-left.svg";
+import BoltDownRight from "~/icons/bolt-down-right.svg";
+import BoltTopLeft from "~/icons/bolt-up-left.svg";
+import BoltTopRight from "~/icons/bolt-up-right.svg";
 </script>
 
 <style scoped>
