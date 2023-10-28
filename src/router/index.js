aa3a9c38edffd8951a59701afa4f124639ada736
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-28 15:48:27 +0530

    added pages in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+// pages
 import Landing from "../pages/index.vue";
+import About from "../pages/about.vue";
+import Projects from "../pages/projects.vue";
+import Contact from "../pages/contact.vue";
+
+// components
 import Maintenance from "../components/maintenance.vue";
 
 const routes = [
   { path: "/", component: Landing, name: "Landing" },
 
   { path: "/maintenance", component: Maintenance, name: "maintenance" },
-  // { path: "/about", component: About },
+
+  { path: "/about", component: About, name: "About Me" },
+
+  { path: "/projects", component: Projects, name: "My projects" },
+
+  { path: "/contact", component: Contact, name: "Contact Me" },
 ];
 
 const router = createRouter({
