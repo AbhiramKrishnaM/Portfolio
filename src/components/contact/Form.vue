a7bba5e750a8ace05718967375fa21716ba7bc96
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-08-14 18:46:24 +0530

    added contact component and view component

---
+++ src/components/contact/Form.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Contact form</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// state
+const contactData = ref({
+  name: "",
+  email: "",
+  message: "",
+  date: "", // Date in ISO format
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/contact/View.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>View section</div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+/**
+ * Instead of adding validations on the form we will add validation here
+ * This should closely watch what is happening on the form component
+ * Format date here
+ */
+
+defineProps({ formObject: { type: Object, required: true } });
+</script>
+
+<style scoped></style>
