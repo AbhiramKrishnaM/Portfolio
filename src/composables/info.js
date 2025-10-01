90272f7f29209075defb0bae66d238770fd3c953
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2025-10-01 14:39:39 +0530

    Add testimonials images and data to info.js

--- src/composables/info.js
+++ src/composables/info.js
@@ -37,8 +37,74 @@ export function useInfo() {
     },
   });
 
+  const testimonials = ref([
+    {
+      name: "Noel Jose TP",
+      designation: "Software Engineer at Dicern Security",
+      text: "Working with Abhiram has been a great experience. He’s passionate, always thinking ahead, and incredibly dependable when it comes to solving problems. His ability to architect solutions and guide through them makes even complex tasks feel straightforward.",
+      image: "https://via.placeholder.com/150",
+    },
+
+    {
+      name: "Libin Mathew Varghese",
+      designation: "Software Engineer at Discern Security",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+
+    {
+      name: "Fasil Karimban Kuzhiyil",
+      designation: "CEO at IOCOD Infotech",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+
+    {
+      name: "Harikrishnan PP",
+      designation: "Frontend Developer at IOCOD Infotech",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+
+    {
+      name: "Mujeeb Parambath",
+      designation: "Software Development Manager at Tenneco Inc",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+
+    {
+      name: "Rohan Puri",
+      designation: "CTO at Discern Security",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+
+    {
+      name: "Yahya Luqman",
+      designation: "Software Development Manager at National Group",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+
+    {
+      name: "David Mundackal",
+      designation: "Software Engineer at IOCOD Infotech",
+      text: "",
+      image: "https://via.placeholder.com/150",
+      show: true,
+    },
+  ]);
+
   return {
     landing,
     footer,
+    testimonials,
   };
 }
